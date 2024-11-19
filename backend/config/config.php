30f33a005f3990f9a7450e72b2a0e2cfab5a85aa
MahSyra <163862120+MahSyra@users.noreply.github.com>
2024-11-20 00:29:11 +0100

    configuration du fichier config.php

---
+++ backend/config/config.php
@@ -0,0 +1,14 @@
+<?php
+// Informations de connexion à la base de données
+define('DB_HOST', 'localhost'); // Adresse du serveur MySQL (ou le nom du conteneur si Docker)
+define('DB_USER', 'root');      // Nom d'utilisateur MySQL
+define('DB_PASS', 'password');  // Mot de passe de l'utilisateur MySQL
+define('DB_NAME', 'mydatabase'); // Nom de la base de données
+// Connexion à la base de données
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("Erreur de connexion : " . $conn->connect_error);
+}
+?>
