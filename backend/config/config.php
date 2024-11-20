069e30c803ecad7743343e8bfe4649da88efd077
MahSyra <163862120+MahSyra@users.noreply.github.com>
2024-11-20 22:39:43 +0100

    Update config.php

--- backend/config/config.php
+++ backend/config/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost'); // Adresse du serveur MySQL (ou le nom du conteneur si Docker)
 define('DB_USER', 'root');      // Nom d'utilisateur MySQL
 define('DB_PASS', 'password');  // Mot de passe de l'utilisateur MySQL
-define('DB_NAME', 'mydatabase'); // Nom de la base de données
+define('DB_NAME', 'databases'); // Nom de la base de données
 // Connexion à la base de données
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
