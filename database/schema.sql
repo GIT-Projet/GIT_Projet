c64fbcefba3bf601a8bd21d7f2a2b20cb53cab02
MahSyra <163862120+MahSyra@users.noreply.github.com>
2024-11-23 02:38:12 +0100

    Update schema.sql
    Mise a jour de la base de donnees

--- database/schema.sql
+++ database/schema.sql
@@ -1,11 +1,12 @@
--- Créer la base de données
-CREATE DATABASE IF NOT EXISTS databases;
-USE databases;
+CREATE DATABASE IF NOT EXISTS my_databases;
+USE my_databases;
 
 -- Table principale : Utilisateurs
 CREATE TABLE IF NOT EXISTS UserAccounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    fullname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL ,
+    username VARCHAR(50) NOT NULL ,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -14,3 +15,4 @@ CREATE TABLE IF NOT EXISTS UserAccounts (
 INSERT INTO UserAccounts (fullname, email, username, password) VALUES
 ('Jean Dupont', 'jean.dupont@example.com', 'jeandupont', '$2y$10$hashedpasswordexample1'),
 ('Alice Martin', 'alice.martin@example.com', 'alicemartin', '$2y$10$hashedpasswordexample2');
+
