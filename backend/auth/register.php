c81d5afd091e442557347089fa6de1a03649a2ce
MahSyra <siyadiarraggmail.com>
2024-12-09 22:51:20 +0100

    modification du register

--- backend/auth/register.php
+++ backend/auth/register.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-ob_start(); // Démarrer la mise en tampon de sortie pour éviter l'erreur "headers already sent"
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
